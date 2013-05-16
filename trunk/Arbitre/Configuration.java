fee090c60f20de8cb17ede15a71a0b0b7922ea7d
gx.hugo <gx.hugo@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-16 22:29:50 +0000

    Ajout d'un hook pour la JVM de shutdown pour fermer l'arbitre proprement

--- trunk/Arbitre/Configuration.java
+++ trunk/Arbitre/Configuration.java
@@ -13,10 +13,10 @@ public class Configuration implements Cloneable
 
     public Configuration(int largeur, int hauteur, Case [][] terrain, Joueur joueurSurConfiguration)
     {
-	this.largeur = largeur;
-	this.hauteur = hauteur;
-	this.terrain = terrain;
-	this.joueurSurConfiguration = joueurSurConfiguration;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.terrain = terrain;
+        this.joueurSurConfiguration = joueurSurConfiguration;
     }
 
     /**
@@ -24,22 +24,22 @@ public class Configuration implements Cloneable
      **/
     public int getLargeur()
     {
-	return largeur;
+        return largeur;
     }
 
     public int getHauteur()
     {
-	return hauteur;
+        return hauteur;
     }
 
     public Case [][] getTerrain()
     {
-	return terrain;
+        return terrain;
     }
 
     public Joueur getJoueurSurConfiguration()
     {
-	return joueurSurConfiguration;
+        return joueurSurConfiguration;
     }
 
     /**
@@ -47,37 +47,37 @@ public class Configuration implements Cloneable
      **/
     public boolean estCoupPossible(Coup coup)
     {
-	int xDepart = coup.getXDepart();
-	int yDepart = coup.getYDepart();
-	int xArrivee = coup.getXArrivee();
-	int yArrivee = coup.getYArrivee();
+        int xDepart = coup.getXDepart();
+        int yDepart = coup.getYDepart();
+        int xArrivee = coup.getXArrivee();
+        int yArrivee = coup.getYArrivee();
 
-	// Reste dans le terrain
-	if (xDepart < 0 || xDepart >= largeur || yDepart < 0 || yDepart >= hauteur)
-	    return false;
+        // Reste dans le terrain
+        if (xDepart < 0 || xDepart >= largeur || yDepart < 0 || yDepart >= hauteur)
+            return false;
 
-	if (xArrivee < 0 || xArrivee >= largeur || yArrivee < 0 || yArrivee >= hauteur)
-	return false;
+        if (xArrivee < 0 || xArrivee >= largeur || yArrivee < 0 || yArrivee >= hauteur)
+            return false;
 
-	if (xDepart == xArrivee && yDepart == yArrivee)
-	return false;
+        if (xDepart == xArrivee && yDepart == yArrivee)
+            return false;
 
-	// Ligne paire
-	if (yArrivee % 2 == 0 && xArrivee >= largeur - 1)
-	    return false;
+        // Ligne paire
+        if (yArrivee % 2 == 0 && xArrivee >= largeur - 1)
+            return false;
 
-	if (yDepart % 2 == 0 && xDepart >= largeur - 1)
-	    return false;
+        if (yDepart % 2 == 0 && xDepart >= largeur - 1)
+            return false;
 
-	// Doit déplacer le pingouin de la config
-	if (terrain[yDepart][xDepart].getJoueurSurCase() != getJoueurSurConfiguration())
-	    return false;
+        // Doit déplacer le pingouin de la config
+        if (terrain[yDepart][xDepart].getJoueurSurCase() != getJoueurSurConfiguration())
+            return false;
 
-	// Sans obstacles avec déplacement possible
-	if (!estDeplacementPossible(coup))
-	    return false;
+        // Sans obstacles avec déplacement possible
+        if (!estDeplacementPossible(coup))
+            return false;
 
-	return true;
+        return true;
     }
 
     /**
@@ -85,48 +85,48 @@ public class Configuration implements Cloneable
      **/
     protected int arrondiSup(int x, int y)
     {
-	if ((double)x/(double)y - x/y >= 0.5)
-	    return x/y + 1;
-	else
-	    return x/y;
+        if ((double)x/(double)y - x/y >= 0.5)
+            return x/y + 1;
+        else
+            return x/y;
     }
 
     protected boolean memeDiagonale(Coup c)
     {
-	if (c.getYDepart() % 2 == 0)
-	    return (c.getXArrivee() - c.getXDepart()) == ((c.getYArrivee() - c.getYDepart()) / 2);
-	else 
-	    return (c.getXArrivee() - c.getXDepart()) == arrondiSup(c.getYArrivee() - c.getYDepart(), 2);
+        if (c.getYDepart() % 2 == 0)
+            return (c.getXArrivee() - c.getXDepart()) == ((c.getYArrivee() - c.getYDepart()) / 2);
+        else 
+            return (c.getXArrivee() - c.getXDepart()) == arrondiSup(c.getYArrivee() - c.getYDepart(), 2);
     }
 
     protected boolean versDroite(Coup c)
     {
-	return c.getYDepart() == c.getYArrivee() && c.getXDepart() < c.getXArrivee();
+        return c.getYDepart() == c.getYArrivee() && c.getXDepart() < c.getXArrivee();
     }
 
     protected boolean versGauche(Coup c)
     {
-	return c.getYDepart() == c.getYArrivee() && c.getXDepart() > c.getXArrivee();
+        return c.getYDepart() == c.getYArrivee() && c.getXDepart() > c.getXArrivee();
     }
 
     protected boolean versBasDroite(Coup c)
     {
-	return memeDiagonale(c) && c.getXArrivee() >= c.getXDepart() && c.getXArrivee() > c.getYDepart();
+        return memeDiagonale(c) && c.getXArrivee() >= c.getXDepart() && c.getXArrivee() > c.getYDepart();
     }
 
     protected boolean versBasGauche(Coup c)
     {
-	return memeDiagonale(c) && c.getXArrivee() <  c.getXDepart() && c.getXArrivee() > c.getYDepart();
+        return memeDiagonale(c) && c.getXArrivee() <  c.getXDepart() && c.getXArrivee() > c.getYDepart();
     }
 
     protected boolean versHautDroite(Coup c)
     {
-	return memeDiagonale(c) && c.getXArrivee() >= c.getXDepart() && c.getXArrivee() < c.getYDepart();
+        return memeDiagonale(c) && c.getXArrivee() >= c.getXDepart() && c.getXArrivee() < c.getYDepart();
     }
 
     protected boolean versHautGauche(Coup c)
     {
-	return memeDiagonale(c) && c.getXArrivee() < c.getXDepart() && c.getXArrivee() < c.getYDepart();
+        return memeDiagonale(c) && c.getXArrivee() < c.getXDepart() && c.getXArrivee() < c.getYDepart();
     }
 
     /**
@@ -134,93 +134,93 @@ public class Configuration implements Cloneable
      **/
     protected boolean estDeplacementPossible(Coup c)
     {
-	int xDepart = c.getXDepart();
-	int yDepart = c.getYDepart();
-	int xArrivee = c.getXArrivee();
-	int yArrivee = c.getYArrivee();
+        int xDepart = c.getXDepart();
+        int yDepart = c.getYDepart();
+        int xArrivee = c.getXArrivee();
+        int yArrivee = c.getYArrivee();
 
-	// Vers la droite
-	if (versDroite(c))
-	{
-	    int l = xDepart;
+        // Vers la droite
+        if (versDroite(c))
+        {
+            int l = xDepart;
 
-	    while (l <= xArrivee)
-	    {
-		if (terrain[yDepart][l].estObstacle())
-		    return false;
-		l++;
-	    }
-	}
-	// Vers la gauche
-	else if (versGauche(c))
-	{
-	    int l = xDepart;
-	    while (l >= xArrivee)
-	    {
-		if (terrain[yDepart][l].estObstacle())
-		    return false;
-		l--;
-	    }
-	}
-	// Vers le haut droit
-	else if (versHautDroite(c))
-	{
-	    int l = c.getXDepart();
+            while (l <= xArrivee)
+            {
+                if (terrain[yDepart][l].estObstacle())
+                    return false;
+                l++;
+            }
+        }
+        // Vers la gauche
+        else if (versGauche(c))
+        {
+            int l = xDepart;
+            while (l >= xArrivee)
+            {
+                if (terrain[yDepart][l].estObstacle())
+                    return false;
+                l--;
+            }
+        }
+        // Vers le haut droit
+        else if (versHautDroite(c))
+        {
+            int l = c.getXDepart();
 	    
-	    for (int k = c.getYDepart() - 1; k > c.getYArrivee(); k--)
-	    {
-		if (k%2 == 0)
-		    l++;
+            for (int k = c.getYDepart() - 1; k > c.getYArrivee(); k--)
+            {
+                if (k%2 == 0)
+                    l++;
 		
-		if (terrain[k][l].estObstacle())
-		    return false;
-	    }		    
-	}
-	// Vers le haut gauche
-	else if (versHautGauche(c))
-	{
-	    int l = c.getXDepart();
+                if (terrain[k][l].estObstacle())
+                    return false;
+            }		    
+        }
+        // Vers le haut gauche
+        else if (versHautGauche(c))
+        {
+            int l = c.getXDepart();
 	    
-	    for (int k = c.getYDepart() - 1; k > c.getYArrivee(); k--)
-	    {
-		if (k%2 == 1)
-		    l--;
+            for (int k = c.getYDepart() - 1; k > c.getYArrivee(); k--)
+            {
+                if (k%2 == 1)
+                    l--;
 		
-		if (terrain[k][l].estObstacle())
-		    return false;
-	    }
-	}
-	// Vers le bas droit
-	else if (versBasDroite(c))
-	{
-	    int l = c.getXDepart();
+                if (terrain[k][l].estObstacle())
+                    return false;
+            }
+        }
+        // Vers le bas droit
+        else if (versBasDroite(c))
+        {
+            int l = c.getXDepart();
 	    
-	    for (int k = c.getYDepart() + 1; k < c.getYArrivee(); k++)
-	    {
-		if (k%2 == 0)
-		    l++;
+            for (int k = c.getYDepart() + 1; k < c.getYArrivee(); k++)
+            {
+                if (k%2 == 0)
+                    l++;
 		
-		if (terrain[k][l].estObstacle())
-		    return false;
-	    }
-	}
-	// Vers le bas gauche
-	else if (versBasGauche(c))
-	{
-	    int l = c.getXDepart();
+                if (terrain[k][l].estObstacle())
+                    return false;
+            }
+        }
+        // Vers le bas gauche
+        else if (versBasGauche(c))
+        {
+            int l = c.getXDepart();
 	    
-	    for (int k = c.getYDepart() + 1; k < c.getYArrivee(); k++)
-	    {
-		if (k%2 == 1)
-		    l--;
+            for (int k = c.getYDepart() + 1; k < c.getYArrivee(); k++)
+            {
+                if (k%2 == 1)
+                    l--;
 		
-		if (terrain[k][l].estObstacle())
-		    return false;
-	    }
+                if (terrain[k][l].estObstacle())
+                    return false;
+            }
 
-	}
+        }
 
-	return true;
+        return true;
     }
 
     /**
@@ -228,124 +228,124 @@ public class Configuration implements Cloneable
      **/
     public Coup [] toutCoupsPossibles()
     {
-	ArrayList<Coup> liste = new ArrayList<Coup>();
+        ArrayList<Coup> liste = new ArrayList<Coup>();
 
-	for (int i = 0; i < hauteur; i++)
-	{
-	    for (int j = 0; j < largeur; j++)
-	    {
-		// Sortie du terrain
-		if (i%2 == 0 && j == largeur - 1)
-		{
-		    j++;
-		    continue;
-		}
+        for (int i = 0; i < hauteur; i++)
+        {
+            for (int j = 0; j < largeur; j++)
+            {
+                // Sortie du terrain
+                if (i%2 == 0 && j == largeur - 1)
+                {
+                    j++;
+                    continue;
+                }
 
-		// Vers la droite
-		{
-		    int l = j - 1;
-		    if (i%2 == 0)
-		    {
-			while (l < largeur - 1 && !terrain[i][l].estObstacle())
-			{
-			    liste.add(new Coup(i, j, i, l));
-			    l++;
-			}
-		    }
-		    else
-		    {
-			while (l < largeur && terrain[i][l].estObstacle())
-			{
-			    liste.add(new Coup(i, j, i, l));
-			    l++;
-			}
-		    }
-		}
+                // Vers la droite
+                {
+                    int l = j - 1;
+                    if (i%2 == 0)
+                    {
+                        while (l < largeur - 1 && !terrain[i][l].estObstacle())
+                        {
+                            liste.add(new Coup(i, j, i, l));
+                            l++;
+                        }
+                    }
+                    else
+                    {
+                        while (l < largeur && terrain[i][l].estObstacle())
+                        {
+                            liste.add(new Coup(i, j, i, l));
+                            l++;
+                        }
+                    }
+                }
 
-		// Vers la gauche
-		{
-		    int l = j - 1;
-		    while (l >= 0 && !terrain[i][l].estObstacle())
-		    {
-			liste.add(new Coup(i, j, i, l));
-			l--;
-		    }
-		}
+                // Vers la gauche
+                {
+                    int l = j - 1;
+                    while (l >= 0 && !terrain[i][l].estObstacle())
+                    {
+                        liste.add(new Coup(i, j, i, l));
+                        l--;
+                    }
+                }
 
-		// Vers le haut droit
-		{
-		    int l = j;
-		    int k = i - 1;
+                // Vers le haut droit
+                {
+                    int l = j;
+                    int k = i - 1;
 
-		    while(k >= 0)
-		    {
-			if (k%2 == 0)
-			    l++;
+                    while(k >= 0)
+                    {
+                        if (k%2 == 0)
+                            l++;
 
-			if (terrain[k][l].estObstacle())
-			    break;
+                        if (terrain[k][l].estObstacle())
+                            break;
 
-			liste.add(new Coup(i, j, k, l));
-		    }
-		}
+                        liste.add(new Coup(i, j, k, l));
+                    }
+                }
 
-		// Vers le haut gauche
-		{
+                // Vers le haut gauche
+                {
 
-		    int l = j;
-		    int k = i - 1;
+                    int l = j;
+                    int k = i - 1;
 
-		    while(k >= 0)
-		    {
-			if (k%2 == 1)
-			    l--;
+                    while(k >= 0)
+                    {
+                        if (k%2 == 1)
+                            l--;
 
-			if (terrain[k][l].estObstacle())
-			    break;
+                        if (terrain[k][l].estObstacle())
+                            break;
 
-			liste.add(new Coup(i, j, k, l));
-		    }
-		}
+                        liste.add(new Coup(i, j, k, l));
+                    }
+                }
 
-		// Vers le bas droit
-		{
-		    int l = j;
-		    int k = i + 1;
+                // Vers le bas droit
+                {
+                    int l = j;
+                    int k = i + 1;
 
-		    while(k < hauteur)
-		    {
-			if (k%2 == 0)
-			    l++;
+                    while(k < hauteur)
+                    {
+                        if (k%2 == 0)
+                            l++;
 
-			if (terrain[k][l].estObstacle())
-			    break;
+                        if (terrain[k][l].estObstacle())
+                            break;
 
-			liste.add(new Coup(i, j, k, l));
-		    }
-		}
+                        liste.add(new Coup(i, j, k, l));
+                    }
+                }
 
-		// Vers le bas gauche
-		{
-		    int l = j;
-		    int k = i + 1;
+                // Vers le bas gauche
+                {
+                    int l = j;
+                    int k = i + 1;
 
-		    while(k < hauteur)
-		    {
-			if (k%2 == 1)
-			    l--;
+                    while(k < hauteur)
+                    {
+                        if (k%2 == 1)
+                            l--;
 
-			if (terrain[k][l].estObstacle())
-			    break;
+                        if (terrain[k][l].estObstacle())
+                            break;
 
-			liste.add(new Coup(i, j, k, l));
-		    }
-		}
-	    }
-	}
+                        liste.add(new Coup(i, j, k, l));
+                    }
+                }
+            }
+        }
 
-	Coup [] tab = new Coup[liste.size()];
-	liste.toArray(tab);
-	return tab;
+        Coup [] tab = new Coup[liste.size()];
+        liste.toArray(tab);
+        return tab;
     }    
 
     /** 
@@ -354,14 +354,14 @@ public class Configuration implements Cloneable
      **/
     public int effectuerCoup(Coup c)
     {
-	System.out.println("effectuerCoup : traiter score");
+        System.out.println("effectuerCoup : traiter score");
 
-	int score = terrain[c.getYDepart()][c.getXDepart()].scorePoisson();
+        int score = terrain[c.getYDepart()][c.getXDepart()].scorePoisson();
 
-	terrain[c.getYDepart()][c.getXDepart()] = new Case(Etat.VIDE, null);
-	terrain[c.getYArrivee()][c.getXArrivee()].setJoueurSurCase(getJoueurSurConfiguration());
+        terrain[c.getYDepart()][c.getXDepart()] = new Case(Etat.VIDE, null);
+        terrain[c.getYArrivee()][c.getXArrivee()].setJoueurSurCase(getJoueurSurConfiguration());
 
-	return score;
+        return score;
     }
 
     /**
@@ -369,13 +369,20 @@ public class Configuration implements Cloneable
      **/
     public Configuration clone()
     {
-	Case [][] terrainCopie = new Case[hauteur][largeur];
-	
-	for (int i = 0; i < hauteur; i++)
-	    for (int j = 0; j < largeur; j++)
-		terrainCopie[i][j] = terrain[i][j].clone();
+        Case [][] terrainCopie = new Case[hauteur][largeur];
+        
+        for (int i = 0; i < hauteur; i++)
+        {
+            for (int j = 0; j < largeur; j++)
+            {
+                if (i%2 == 0 && j == largeur - 1)
+                    continue;
 
-	return new Configuration(largeur, hauteur, terrainCopie, joueurSurConfiguration);
+                terrainCopie[i][j] = terrain[i][j].clone();
+            }
+        }
+
+        return new Configuration(largeur, hauteur, terrainCopie, joueurSurConfiguration);
     }
     
 
@@ -384,24 +391,27 @@ public class Configuration implements Cloneable
      **/
     public String getSerialize()
     {
-	String result = "";
+        String result = "";
 
-	for (int i = 0; i < hauteur; i++)
-	{
-	    for (int j = 0; j < largeur; j++)
-	    {
-		Joueur joueur = terrain[i][j].getJoueurSurCase();
+        for (int i = 0; i < hauteur; i++)
+        {
+            for (int j = 0; j < largeur; j++)
+            {
+                if (i%2 == 0 && j == largeur - 1)
+                    continue;
 
-		if (joueur == null)
-		    result += "0";
-		else
-		    result += String.valueOf(ArbitreManager.instance.getPosition(joueur));
+                Joueur joueur = terrain[i][j].getJoueurSurCase();
 
-		result += String.valueOf(terrain[i][j].scorePoisson());
-	    }
-	}
+                if (joueur == null)
+                    result += "0";
+                else
+                    result += String.valueOf(ArbitreManager.instance.getPosition(joueur));
+
+                result += String.valueOf(terrain[i][j].scorePoisson());
+            }
+        }
 	
-	return result;
+        return result;
     }
 
     /**
@@ -410,36 +420,36 @@ public class Configuration implements Cloneable
      **/
     public boolean setSerialized(String result)
     {   
-	if (hauteur * largeur != result.length() * 2)
-	    return false;
+        if (hauteur * largeur != result.length() * 2)
+            return false;
 
-	terrain = new Case[hauteur][largeur];
-	int compteur = 0;
+        terrain = new Case[hauteur][largeur];
+        int compteur = 0;
 
-	for (int i = 0; i < hauteur; i++)
-	{
-	    for (int j = 0; j < largeur; j++)
-	    {
-		Joueur joueur = null;
-		Etat e = Etat.VIDE;
-		if (result.charAt(compteur) != '0')
-		    joueur = ArbitreManager.instance.getJoueurParPosition(Integer.valueOf(result.charAt(compteur)));
+        for (int i = 0; i < hauteur; i++)
+        {
+            for (int j = 0; j < largeur; j++)
+            {
+                Joueur joueur = null;
+                Etat e = Etat.VIDE;
+                if (result.charAt(compteur) != '0')
+                    joueur = ArbitreManager.instance.getJoueurParPosition(Integer.valueOf(result.charAt(compteur)));
 
-		compteur++;
+                compteur++;
 
-		if (result.charAt(compteur) == '1')
-		    e = Etat.UN_POISSON;
-		else if (result.charAt(compteur) == '2')
-		    e = Etat.DEUX_POISSONS;
-		else if (result.charAt(compteur) == '3')
-		    e = Etat.TROIS_POISSONS;
-		else
-		    e = Etat.VIDE;
+                if (result.charAt(compteur) == '1')
+                    e = Etat.UN_POISSON;
+                else if (result.charAt(compteur) == '2')
+                    e = Etat.DEUX_POISSONS;
+                else if (result.charAt(compteur) == '3')
+                    e = Etat.TROIS_POISSONS;
+                else
+                    e = Etat.VIDE;
 
-		terrain[i][j] = new Case(e, joueur);
-	    }
-	}
+                terrain[i][j] = new Case(e, joueur);
+            }
+        }
 
-	return true;
+        return true;
     }
 }
