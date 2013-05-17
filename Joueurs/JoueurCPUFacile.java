8229846300de026c39d56832a99fb61d817773d0
doudfs <doudfs@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-17 12:41:38 +0000

    Ajout cpu facile

---
+++ Joueurs/JoueurCPUFacile.java
@@ -0,0 +1,34 @@
+package Joueurs;
+import Arbitre.*;
+import java.util.Random;
+
+public class JoueurCPUFacile extends Joueur
+{    
+    /**
+     * Choisi une des cases qui contient le plus de poisson
+     **/
+	public Coup coupSuivant()
+    {
+		Coup [] coupPossible = ArbitreManager.instance.getConfiguration().toutCoupsPossibles();
+		Case [][] terrain = ArbitreManager.instancce.getConfiguration().getTerrain();
+		Case tmp;
+		int max = 0, imax = -1;
+		
+		for (int i = 0; i < coupPossible.length; i++) {
+			tmp = terrain[coupPossible[i].getXArrivee()][coupPossible.getYArrivee()];
+			if (tmp.scorePoisson() > max) {
+				imax = i;
+				max = tmp.scorePoisson();
+				if (max > 2) {
+					break;
+				}
+			}
+		}
+		return coupPossible[imax];
+	}
+
+    public static String getType()
+    {
+        return "CPU_Facile";
+    }
+}
