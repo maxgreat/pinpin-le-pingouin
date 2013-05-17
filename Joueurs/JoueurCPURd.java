5153885ece64d4b9e4bf0dce79183901776c08fa
doudfs <doudfs@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-17 13:31:40 +0000

    Ajout placement sur IA Rd

--- Joueurs/JoueurCPURd.java
+++ Joueurs/JoueurCPURd.java
@@ -7,13 +7,18 @@ public class JoueurCPURd extends Joueur
     /**
      * Choisi simplement un coup au hasard
      **/
-	public Coup coupSuivant()
-    {
-		Coup [] coupPossible = ArbitreManager.instance.getConfiguration().toutCoupsPossibles();
+	public Coup coupSuivant() {
 		Random r = new Random();
+		// Phase de placement
+		if (ArbitreManager.instance.getMode() == ModeDeJeu.POSE_PINGOUIN) {
+			Coup [] placementPossible = ArbitreManager.instance.getConfiguration().toutPlacementPossible();
+			return placementPossible[r.nextInt(placementPossible.length)];
+		}
+		// Phase de jeu
+		Coup [] coupPossible = ArbitreManager.instance.getConfiguration().toutCoupsPossibles();
 		return coupPossible[r.nextInt(coupPossible.length)];
 	}
-
+	
     public static String getType()
     {
         return "CPU_Random";
