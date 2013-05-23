89745d8c95b760448820a7dfdb154689c72ade0e
pi.banwarth <pi.banwarth@gmail.com>
2013-05-23 09:39:22 +0000

    manque plus que partie perso

--- Interface/Graphique/InterfaceGraphique.java
+++ Interface/Graphique/InterfaceGraphique.java
@@ -17,7 +17,7 @@ public class InterfaceGraphique extends Interface
     public Fond fond;
     protected AireDeJeu aire;
     protected JFrame frame;
-    GridBagConstraints c1,c2,c3,c4; 
+    GridBagConstraints banniere,joueur1,joueur2,joueur3,joueur4,menu,quitter,jouer; 
     public void run(String [] arguments)
     {		
 		Dimension Dim = new Dimension(700,500);
@@ -31,8 +31,9 @@ public class InterfaceGraphique extends Interface
 		// on met un layout de grid bag sur fond
 		fond.setLayout(new GridBagLayout());
 
-		fond.add(Panels[0],c2);
-		fond.add(ban,c1);
+		fond.add(Panels[0],menu);
+		fond.add(Panels[5],quitter);
+		fond.add(ban,banniere);
 		frame.add(fond);
 		frame.setResizable(true);
 		frame.setMinimumSize(Dim);
@@ -77,7 +78,7 @@ public class InterfaceGraphique extends Interface
 	pan.add(b1);
 		if(S.compareTo("Revenir")==0)
 		{
-			fond.add(pan,c4);
+			fond.add(pan,joueur3);
 		}
 	pan.setOpaque(false);	
 	panel.add(pan);
@@ -94,33 +95,43 @@ public class InterfaceGraphique extends Interface
 	addBouton(Panels[0],"Partie Rapide");
 	addBouton(Panels[0],"Partie Personalisé");	
 	addBouton(Panels[0],"Options");
-	addBouton(Panels[0],"Quitter");
+
 	// le panel 1 correspond a la page d'options
 	Panels[1].setLayout(new BoxLayout(Panels[1], BoxLayout.PAGE_AXIS));
 	Panels[1].setOpaque(false);
 	addBouton(Panels[1],"Gestion de profil");
 	addBouton(Panels[1],"Classements");			
 	// le panel 2 est le panel correspondant a la partie personalisé
-	Panels[2].setOpaque(false);
-	Panels[2].setLayout(new GridLayout( 7 , 2) );
-	addBouton(Panels[2],"nom du joueur1");
-	addBouton(Panels[2],"nom du joueur2");		
-	addBouton(Panels[2],"niveau du joueur1");
-	addBouton(Panels[2],"niveau du joueur2");
-	addBouton(Panels[2],"avatar joueur 1");		
-	addBouton(Panels[2],"avatar joueur 2");
-	addBouton(Panels[2],"nom du joueur3");
-	addBouton(Panels[2],"nom du joueur4");		
-	addBouton(Panels[2],"niveau du joueur3");
-	addBouton(Panels[2],"niveau du joueur4");
-	addBouton(Panels[2],"avatar joueur 3");		
-	addBouton(Panels[2],"avatar joueur 4");
+	Panels[6].setOpaque(false);
+	Panels[6].setLayout(new GridLayout( 3 , 1) );
+	addBouton(Panels[6],"nom du joueur1");
+	addBouton(Panels[6],"niveau du joueur1");
+	addBouton(Panels[6],"avatar joueur 1");
+
+	Panels[7].setOpaque(false);
+	Panels[7].setLayout(new GridLayout( 3 , 1) );
+	addBouton(Panels[7],"nom du joueur2");
+	addBouton(Panels[7],"niveau du joueur2");
+	addBouton(Panels[7],"avatar joueur 2");
+
+	Panels[8].setOpaque(false);
+	Panels[8].setLayout(new GridLayout( 3 , 1) );
+	addBouton(Panels[8],"nom du joueur3");
+	addBouton(Panels[8],"niveau du joueur3");
+	addBouton(Panels[8],"avatar joueur 3");
+
+	Panels[9].setOpaque(false);
+	Panels[9].setLayout(new GridLayout( 3 , 1) );
+	addBouton(Panels[9],"nom du joueur4");
+	addBouton(Panels[9],"niveau du joueur4");
+	addBouton(Panels[9],"avatar joueur 4");
 	// le panel 3 est le panel correspondant au gestion de profil
 	Panels[3].setLayout(new BoxLayout(Panels[3], BoxLayout.PAGE_AXIS));
 	Panels[3].setOpaque(false);
 	// le panel 4 est le panel correspondant au classement
 	Panels[4].setLayout(new BoxLayout(Panels[4], BoxLayout.PAGE_AXIS));
 	Panels[4].setOpaque(false);
+	//le panel correspondant au deux boutons quitter et revenir
 	Panels[5].setLayout(new BoxLayout(Panels[5], BoxLayout.PAGE_AXIS));
 	Panels[5].setOpaque(false);
 	addBouton(Panels[5],"Revenir");
@@ -137,43 +148,92 @@ public class InterfaceGraphique extends Interface
     public void initialiserContraintes()
 	{
 	fond = new Fond("Image_fon.jpg",frame);
-	c1 = new GridBagConstraints();
-	c2 = new GridBagConstraints();
-	c3 = new GridBagConstraints();
-	c4 = new GridBagConstraints();
-	c1.gridx = 0;
-	c1.gridy =0;
-	c1.gridwidth =3;
-	c1.gridheight =1;
-	c1.anchor =GridBagConstraints.FIRST_LINE_START;
-	c1.fill =GridBagConstraints.BOTH;
-	c1.weightx=1;
-	c1.weighty=1;	
-	c1.ipadx = 0;
-	c1.ipady = 0;
+	banniere = new GridBagConstraints();
+	joueur1 = new GridBagConstraints();
+	joueur2 = new GridBagConstraints();
+	joueur3 = new GridBagConstraints();
+	joueur4 = new GridBagConstraints();
+	menu = new GridBagConstraints();
+	quitter = new GridBagConstraints();
+	jouer = new GridBagConstraints();
+	// la banniere
+	banniere.gridx = 0;
+	banniere.gridy =0;
+	banniere.gridwidth =7;
+	banniere.gridheight =2;
+	banniere.anchor =GridBagConstraints.FIRST_LINE_START;
+	banniere.fill =GridBagConstraints.BOTH;
+	banniere.weightx=1;
+	banniere.weighty=1;	
 	
-	c2.gridx =2;
-	c2.gridy =1;
-	c2.gridwidth =1;
-	c2.gridheight =1;
-	c2.weightx=1;
-	c2.weighty=1;
-	c2.anchor = GridBagConstraints.CENTER;
-	c2.fill =GridBagConstraints.BOTH;
+	//le joueur 1 dans la configuration
+	joueur1.gridx =1;
+	joueur1.gridy =3;
+	joueur1.gridwidth =2;
+	joueur1.gridheight =2;
+	joueur1.weightx=0;
+	joueur1.weighty=0;
+	joueur1.anchor = GridBagConstraints.CENTER;
+	joueur1.fill =GridBagConstraints.NONE;
 
-	c3.gridx =2;
-	c3.gridy =4;
-	c3.gridwidth =1;
-	c3.gridheight =1;
-	c3.anchor =GridBagConstraints.LAST_LINE_END;
-	c3.fill =GridBagConstraints.BOTH;
+	//le joueur 2 dans la configuration
+	joueur2.gridx =4;
+	joueur2.gridy =3;
+	joueur2.gridwidth =2;
+	joueur2.gridheight =2;
+	joueur2.weightx=0;
+	joueur2.weighty=0;
+	joueur2.anchor = GridBagConstraints.CENTER;
+	joueur2.fill =GridBagConstraints.NONE;
 
-	c4.gridx =2;
-	c4.gridy =4;
-	c4.gridwidth =1;
-	c4.gridheight =1;
-	c4.anchor = GridBagConstraints.LAST_LINE_START;
-	c4.fill =GridBagConstraints.BOTH;
+	//le joueur 3 dans la configuration
+	joueur3.gridx =1;
+	joueur3.gridy =6;
+	joueur3.gridwidth =2;
+	joueur3.gridheight =2;
+	joueur3.weightx=0;
+	joueur3.weighty=0;
+	joueur3.anchor = GridBagConstraints.CENTER;
+	joueur3.fill =GridBagConstraints.NONE;
+
+	//le joueur 4 dans la configuration
+	joueur4.gridx =4;
+	joueur4.gridy =6;
+	joueur4.gridwidth =2;
+	joueur4.gridheight =2;
+	joueur4.weightx=0;
+	joueur4.weighty=0;
+	joueur4.anchor = GridBagConstraints.CENTER;
+	joueur4.fill =GridBagConstraints.NONE;
+	// les menu
+	menu.gridx =1;
+	menu.gridy =3;
+	menu.gridwidth =3;
+	menu.gridheight =6;
+	menu.weightx=0;
+	menu.weighty=0;
+	menu.anchor = GridBagConstraints.CENTER;
+	//menu.fill =GridBagConstraints.BOTH;
+	// quitter revenir
+	quitter.gridx =1;
+	quitter.gridy =9;
+	quitter.gridwidth =1;
+	quitter.gridheight =1;
+	quitter.weightx=0.5;
+	quitter.weighty=0.5;
+	quitter.anchor = GridBagConstraints.CENTER;
+	quitter.fill =GridBagConstraints.NONE;	
+	// jouer
+	jouer.gridx =5;
+	jouer.gridy =9;
+	jouer.gridwidth =1;
+	jouer.gridheight =1;
+	jouer.weightx=0;
+	jouer.weighty=0;
+	jouer.anchor = GridBagConstraints.CENTER;
+	jouer.fill =GridBagConstraints.NONE;
+
+
 
 	}
     public void afficherPanel(String S)
@@ -184,8 +244,8 @@ public class InterfaceGraphique extends Interface
 		if (S.compareTo("Menu Principal") == 0)
 		{	
 			removeFrame(frame);
-			fond.add(Panels[0],c2);
-			
+			fond.add(Panels[0],menu);
+			fond.add(Panels[5],quitter);
 			frame.add(fond);
 			frame.repaint();
 			frame.pack();				
@@ -215,8 +275,8 @@ public class InterfaceGraphique extends Interface
 		{
 			removeFrame(frame);
 			Old_page.addFirst("Options");
-			fond.add(Panels[1],c2);
-			fond.add(Panels[5],c4);
+			fond.add(Panels[1],menu);
+			fond.add(Panels[5],quitter);
 			frame.add(fond);
 			frame.repaint();
 			frame.pack();			
@@ -231,8 +291,12 @@ public class InterfaceGraphique extends Interface
 		{
 			removeFrame(frame);
 			Old_page.addFirst("Options");
-			fond.add(Panels[2],c2);
-			fond.add(Panels[5],c4);
+			fond.add(Panels[2],menu);
+			fond.add(Panels[5],quitter);
+			fond.add(Panels[6],joueur1);
+			fond.add(Panels[7],joueur2);
+			fond.add(Panels[8],joueur3);
+			fond.add(Panels[9],joueur4);
 			frame.add(fond);
 			frame.repaint();
 			frame.pack();	
@@ -245,8 +309,8 @@ public class InterfaceGraphique extends Interface
 		{
 			removeFrame(frame);
 			Old_page.addFirst("Gestion de profil");
-			fond.add(Panels[3],c2);
-			fond.add(Panels[5],c4);
+			fond.add(Panels[3],menu);
+			fond.add(Panels[5],quitter);
 			frame.add(fond);
 			frame.repaint();
 			frame.pack();	
@@ -255,8 +319,8 @@ public class InterfaceGraphique extends Interface
 		{
 			removeFrame(frame);
 			Old_page.addFirst("Classement");
-			fond.add(Panels[4],c2);
-			fond.add(Panels[5],c4);
+			fond.add(Panels[4],menu);
+			fond.add(Panels[5],quitter);
 			frame.add(fond);
 			frame.repaint();
 			frame.pack();	
