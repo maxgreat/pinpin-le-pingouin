e3e841b4095b38f5b8dfd20a9047e16a25b3a6e6
pi.banwarth@gmail.com <pi.banwarth@gmail.com@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-29 14:51:30 +0000

    les ecouteurs devrait marcher

--- trunk/Interface/Graphique/EcouteurDeBox.java
+++ trunk/Interface/Graphique/EcouteurDeBox.java
@@ -9,15 +9,32 @@ import java.net.URL;
 
 public class EcouteurDeBox implements ActionListener{
 	MenuPerso menu;
-	
-	public EcouteurDeBox(MenuPerso menu){
+	String S;
+	JComboBox Comb;
+	public EcouteurDeBox(MenuPerso menu , String S, JComboBox Comb){
 		this.menu = menu;
+		this.S = S;
+		this.Comb = Comb;
 	}
+	
 
 	public void actionPerformed(ActionEvent e){
-		
-
-
+	if(S.compareTo("joueur1") == 0)
+		menu.joueur1 = (String)Comb.getSelectedItem();
+	if(S.compareTo("joueur2") == 0)
+		menu.joueur2 = (String)Comb.getSelectedItem();
+	if(S.compareTo("joueur3") == 0)
+		menu.joueur3 = (String)Comb.getSelectedItem();
+	if(S.compareTo("joueur4") == 0)
+		menu.joueur4 =(String) Comb.getSelectedItem();
+	if(S.compareTo("niveau1") == 0)
+		menu.niveau1 = (String)Comb.getSelectedItem();
+	if(S.compareTo("niveau2") == 0)
+		menu.niveau2 = (String)Comb.getSelectedItem();
+	if(S.compareTo("niveau3") == 0)
+		menu.niveau3 = (String)Comb.getSelectedItem();
+	if(S.compareTo("niveau4") == 0)
+		menu.niveau4 = (String)Comb.getSelectedItem();
 	}
 
 }
