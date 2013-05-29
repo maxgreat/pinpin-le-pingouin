7e60f8eabdf2c3de1e14711675b24d08f5ce374f
maxime.portaz <maxime.portaz@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-29 08:30:09 +0000

    correction coquille

---
+++ Interface/Graphique/EcouteurDeBox.java
@@ -0,0 +1,23 @@
+package Interface.Graphique;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.IOException;
+import java.net.URL;
+
+public class EcouteurDeBox implements ActionListener{
+	MenuPerso menu;
+	
+	public EcouteurDeBox(MenuPerso menu){
+		this.menu = menu;
+	}
+
+	public void actionPerformed(ActionEvent e){
+		
+
+
+	}
+
+}
