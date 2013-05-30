b93e65350d069115b8407010e2790d1c6d703206
pi.banwarth@gmail.com <pi.banwarth@gmail.com@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-30 14:36:57 +0000

    debug

--- trunk/Interface/Graphique/EcouteurDeBox.java
+++ trunk/Interface/Graphique/EcouteurDeBox.java
@@ -13,9 +13,9 @@ public class EcouteurDeBox implements ActionListener{
 	JComboBox Comb;
 	MenuPerso perso;
 	String current;
-	JFrame frame
+
 	int i;
-	public EcouteurDeBox(InterfaceGraphique inter , String S, JComboBox Comb,MenuPerso perso,String current,int i,JFrame frame){
+	public EcouteurDeBox(InterfaceGraphique inter , String S, JComboBox Comb,MenuPerso perso,String current,int i){
 		this.inter = inter;
 		this.S = S;
 		this.i = i;
@@ -108,7 +108,7 @@ public class EcouteurDeBox implements ActionListener{
 		{
 		inter.niveau4 = (String)Comb.getSelectedItem();
 		}
-	
+	inter.afficherPanel("Partie Personalisée");
 	}
 
 }
