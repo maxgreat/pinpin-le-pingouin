d6b48b7e96f3ef76e3dc6a232264b36edbadeb0a
pi.banwarth@gmail.com <pi.banwarth@gmail.com@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-31 09:29:13 +0000

    ajout d'une classe dans interface graphique

---
+++ trunk/Interface/Graphique/SelectionJoueur.java
@@ -0,0 +1,29 @@
+
+package Interface.Graphique;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.IOException;
+import java.net.URL;
+public class SelectionJoueur{
+
+
+JComboBox names;
+JComboBox difficultes;
+
+public SelectionJoueur(JComboBox names , JComboBox difficultes)
+	{
+	this.names = names;
+	this.difficultes =difficultes; 
+	}
+
+
+
+
+
+
+
+}
+
