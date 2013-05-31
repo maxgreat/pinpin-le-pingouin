e761b6777c7069849303b558d2830ae43f476b00
doudfs <doudfs@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-05-31 09:56:42 +0000

    Ajout d'un joueur universel -> possibilité de changer de niveau en cours de partie avec setLevel

--- trunk/Joueurs/JoueurCPUPhase.java
+++ trunk/Joueurs/JoueurCPUPhase.java
@@ -6,9 +6,16 @@ import java.util.*;
 public class JoueurCPUPhase extends Joueur {
       	
 	JoueurCPURandomizable cpuRdizable;
+	Joueur player;
 
 	public JoueurCPUPhase() {
 		this.cpuRdizable = new JoueurCPURandomizable(this);
+		this.player = this;
+	}
+
+	public JoueurCPUPhase(Joueur j) {
+		this.cpuRdizable = new JoueurCPURandomizable(j);
+		this.player = j;
 	}
 
 	public static String getType() {
@@ -21,12 +28,13 @@ public class JoueurCPUPhase extends Joueur {
 			 * Phase de placement
 			 * Utilisation du CPUFacile
 			 **/
+			System.out.println("Facile");
 			JoueurCPUFacile cpuFacile = new JoueurCPUFacile();
 			return cpuFacile.coupSuivant();
 		} else {
 			Configuration c = ArbitreManager.instance.getConfiguration();	
 			Boolean finish = true;
-			Couple [] pingouins = c.coordPingouins(this);
+			Couple [] pingouins = c.coordPingouins(this.player);
 			for (int i = 0; i < pingouins.length && finish; i++) {
 				Couple couple = c.estIlot(pingouins[i].getX(), pingouins[i].getY(), new ArrayList<Couple>());
 				if (couple.getX() == -1) {
@@ -43,7 +51,8 @@ public class JoueurCPUPhase extends Joueur {
 			 * Phase centrale
 			 * Utilisation du MinimaxIncremental
 			 **/
-			JoueurCPUMinimaxIncremental cpuMinimax = new JoueurCPUMinimaxIncremental(this, false);
+			System.out.println("Incremental");
+			JoueurCPUMinimaxIncremental cpuMinimax = new JoueurCPUMinimaxIncremental(this.player, false);
 			Coup coup = cpuMinimax.coupSuivant();
 			System.out.println(coup);
 			return coup;
