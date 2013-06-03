9bbafc64406d4477c82af2555465a6407fda1e48
pi.banwarth@gmail.com <pi.banwarth@gmail.com@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-06-03 11:46:37 +0000

    modif post reunion

--- trunk/Interface/Graphique/SelectionJoueurs.java
+++ trunk/Interface/Graphique/SelectionJoueurs.java
@@ -31,7 +31,7 @@ String nomJoueurOld = new String();
 //on a besoin de menu perso pour
 MenuPerso mp;
 //les combobox
-JComboBox names;
+JPanel names;
 JComboBox diff;
 int menu;
 InterfaceGraphique inter;
@@ -52,45 +52,28 @@ public void selectionJoueur()
 	{
 
 	Object [] test = joueurs.toArray();
-	names = new JComboBox(test);
+	names = new JPanel();
 	diff = new JComboBox(difficultees.toArray());
 	diff.setVisible(false);
-	if(menu == 3 || menu == 4)
-		names.setSelectedItem("Aucun");
-	if(menu == 1)
-		names.setSelectedItem("Atos");
+	names.setLayout(new GridLayout(0,3));
+	JButton b1 = new JButton("HUMAIN");
+	JButton b2 = new JButton("CPU");
+	JButton b3 = new JButton("AUCUN");
+	names.add(b1);
+	names.add(b2);
+	names.add(b3);
+
 	if(menu == 2)
 		{
-		names.setSelectedItem("Ordinateur");
 		diff.setVisible(true);
 		}
 		diff.setSelectedItem("Intermediaire");
-	names.addActionListener(new EcouteurDeBox(this,true,mp.inter,menu));
+	//names.addActionListener(new EcouteurDeBox(this,true,mp.inter,menu));
 	diff.addActionListener(new EcouteurDeBox(this,false,mp.inter,menu));
 	
 	
 	joueur.setLayout(new BoxLayout(joueur, BoxLayout.PAGE_AXIS));
 	
-	if(menu == 1)
-	{
-		Img J1 = new Img("j1.png");
-		joueur.add(J1);	
-	}
-	if(menu == 2)
-	{
-		Img J2 = new Img("j2.png");
-		joueur.add(J2);	
-	}
-	if(menu ==3)
-	{
-		Img J3 = new Img("j3.png");
-		joueur.add(J3);
-	}
-	if(menu == 4)
-	{
-		Img J4 = new Img("j4.png");
-		joueur.add(J4);	
-	}
 	joueur.add(names);
 	joueur.add(diff);
 	joueur.setOpaque(false);
