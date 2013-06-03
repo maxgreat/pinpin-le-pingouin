e7d46fd2b8ef86ec6bb50e3976726b8f2bf5ef7b
maxime.portaz <maxime.portaz@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-06-03 08:10:33 +0000

    ajout commentaire et correction

--- Interface/Graphique/AireDeJeu.java
+++ Interface/Graphique/AireDeJeu.java
@@ -12,11 +12,14 @@ import Arbitre.Regles.*;
 
 public class AireDeJeu extends JComponent
 {
-    Hexagone tabCase;
+    private Hexagone tabCase;
     JFrame frame;
     JPanel pan;
     InterfaceGraphique inter;
-    Point coupPrec;
+    private Point coupPrec;
+    
+    private Point centreAide = null;
+    private Point centreSuggestion = null;
 
     boolean showDialog = true;
 	boolean popup1 = true;
@@ -186,7 +189,7 @@ public class AireDeJeu extends JComponent
 				{ 
 					for(int i = 1; i <= n; i++)
 					{
-						drawable.drawImage(imageJoueur1, (i-1)*(int)margeGauche/4, (int)margeHaut, 2*(int)margeGauche/4, 2*(int)margeHaut/4, null);
+						drawable.drawImage(imageJoueur1, (i-1)*(int)margeGauche/4, (int)margeHaut, (int)margeGauche/2, (int)margeHaut/2, null);
 					}
 					break;
 				}	
@@ -194,7 +197,7 @@ public class AireDeJeu extends JComponent
 				{ 
 					for(int i = 1; i <= n; i++)
 					{
-						drawable.drawImage(imageJoueur2, largeur-((int)margeGauche - (i-1)*(int)margeGauche/4), (int)margeHaut, 2*(int)margeGauche/4, 2*(int)margeHaut/4, null);
+						drawable.drawImage(imageJoueur2, largeur-((int)margeGauche - (i-1)*(int)margeGauche/4), (int)margeHaut, (int)margeGauche/2, (int)margeHaut/2, null);
 					}
 					break;
 				}	
@@ -240,7 +243,7 @@ public class AireDeJeu extends JComponent
     public void paintComponent(Graphics g)
     {
     	
-	Graphics2D drawable = (Graphics2D) g;
+		Graphics2D drawable = (Graphics2D) g;
         
         Arbitre arbitre = ArbitreManager.instance;
 			hauteur = this.getHeight();
@@ -287,7 +290,7 @@ public class AireDeJeu extends JComponent
             // on retourne au menu principal
 
           
-		        //default title and icon
+		    //default title and icon
 			if (showDialog)
 			{
 				System.out.println(s);
@@ -323,236 +326,224 @@ public class AireDeJeu extends JComponent
 					} catch (java.beans.PropertyVetoException e) {}
 				showDialog = false;
 			}
-			/*
-			TODO
-			s = "placer vos pingouins sur les cases rouges";
-			if (popup1)
-			{
-				System.out.println(s);
-			
-				java.awt.EventQueue.invokeLater(new Runnable()
-				{
-					private String message;
-					public void run() 
-					{
-					JOptionPane.showMessageDialog(null, message, "Informations", JOptionPane.INFORMATION_MESSAGE);
-					}
-
-					private Runnable init(String msg)
-					{
-					message = msg;
-					return this;
-					}  
-				}.init(s));
-				popup1 = false;
-			}
-			*/
 	
 	
         }//fin traitement partie finie 
         
        //partie en cours
+    
+        //Recuperation de la configuration
+        Configuration config = arbitre.getConfiguration();
+        //Recuperation du Terrain
+        Case [][] c = config.getTerrain();
         
-            //Recuperation de la configuration
-            Configuration config = arbitre.getConfiguration();
-            //Recuperation du Terrain
-            Case [][] c = config.getTerrain();
-            
-            //Recuperation de la hauteur et de la largeur	
+        //Recuperation de la hauteur et de la largeur	
+	
+		//redefinition des marges
+		margeHaut = (double)hauteur/8.0;
+		margeGauche = (double)largeur/8.0;
+		margeDroite = margeGauche;
+		margeBas = margeHaut;
 		
-			//redefinition des marges
-			margeHaut = (double)hauteur/8.0;
-			margeGauche = (double)largeur/8.0;
-			margeDroite = margeGauche;
-			margeBas = margeHaut;
-			
-			//calcul des rayons
-			rayonH = ((double)hauteur - margeHaut - margeBas) / 12.5;
-			rayonL = ((double)largeur - margeGauche - margeDroite) / 16.0;
-			
-			drawable.drawImage(fondEau, 0,0, largeur, hauteur, null);
-			
-			
-			//dessin des carrés de joueur
-			afficherCarres(drawable, (int)margeGauche, (int)margeHaut);
-			
-			//maj du tableau case
-			tabCase.setTab(rayonH, rayonL, margeHaut, margeGauche, largeur, hauteur);
-			Joueur joueur;
-			
-			//Si on est en mode pose pingouins
-			if(ArbitreManager.instance.getMode() == ModeDeJeu.POSE_PINGOUIN){
-					//demande à l'utilisateur de placer les pingouins
-					drawable.drawImage(placement, largeur/4 + (int)rayonL*3, hauteur-(int)rayonH-10, largeur/4 + 30, 2*(int)rayonH/3, null);
-					//affichage des pingouins sous les cases des joueurs
-					afficherPingouins(drawable);
-			}
-			
-			
-			
-			//Dessin des boutons
-			
-			//en haut
-			//drawable.drawImage(info,largeur/4, 0, (int)tabCase.largeur, (int)tabCase.hauteur,null);
-			//drawable.drawImage(suggest,3*largeur/4-(int)tabCase.largeur, 0, (int)tabCase.largeur,(int)tabCase.hauteur,null);
-			
-			//sur les cotés
-			drawable.drawImage(info,0, hauteur/2, (int)tabCase.largeur, (int)tabCase.hauteur,null);
-			drawable.drawImage(suggest,largeur-(int)tabCase.largeur, hauteur/2, (int)tabCase.largeur,(int)tabCase.hauteur,null);
-			
-			
-			
-			//bouton menu
-			drawable.drawImage(boutonMenu, largeur/2-35, 0, 70,50,null);
-			//bouton annuler
-			drawable.drawImage(boutonAnnuler, largeur/4, hauteur-(int)rayonH-10, (int)rayonL*2+20, (int)rayonH, null);
-			//bouton refaire
-			drawable.drawImage(boutonRefaire, 3*largeur/4-(int)rayonL, hauteur-(int)rayonH-10, (int)rayonL*2+20, (int)rayonH, null);
-	
-	
+		//calcul des rayons
+		rayonH = ((double)hauteur - margeHaut - margeBas) / 12.5;
+		rayonL = ((double)largeur - margeGauche - margeDroite) / 16.0;
+		
+		drawable.drawImage(fondEau, 0,0, largeur, hauteur, null);
+		
+		
+		//dessin des carrés de joueur
+		afficherCarres(drawable, (int)margeGauche, (int)margeHaut);
+		
+		//maj du tableau case
+		tabCase.setTab(rayonH, rayonL, margeHaut, margeGauche, largeur, hauteur);
+		Joueur joueur;
+		
+		//Si on est en mode pose pingouins
+		if(ArbitreManager.instance.getMode() == ModeDeJeu.POSE_PINGOUIN){
+				//demande à l'utilisateur de placer les pingouins
+				drawable.drawImage(placement, largeur/4 + (int)rayonL*3, hauteur-(int)rayonH-10, largeur/4 + 30, 2*(int)rayonH/3, null);
+				//affichage des pingouins sous les cases des joueurs
+				afficherPingouins(drawable);
+		}
+		
+		
+		
+		//Dessin des boutons
+		
+		//en haut
+		//drawable.drawImage(info,largeur/4, 0, (int)tabCase.largeur, (int)tabCase.hauteur,null);
+		//drawable.drawImage(suggest,3*largeur/4-(int)tabCase.largeur, 0, (int)tabCase.largeur,(int)tabCase.hauteur,null);
+		
+		//sur les cotés
+		drawable.drawImage(info,0, hauteur/2, (int)tabCase.largeur, (int)tabCase.hauteur,null);
+		drawable.drawImage(suggest,largeur-(int)tabCase.largeur, hauteur/2, (int)tabCase.largeur,(int)tabCase.hauteur,null);
+		
+		
+		
+		//bouton menu
+		drawable.drawImage(boutonMenu, largeur/2-35, 0, 70,50,null);
+		//bouton annuler
+		drawable.drawImage(boutonAnnuler, largeur/4, hauteur-(int)rayonH-10, (int)rayonL*2+20, (int)rayonH, null);
+		//bouton refaire
+		drawable.drawImage(boutonRefaire, 3*largeur/4-(int)rayonL, hauteur-(int)rayonH-10, (int)rayonL*2+20, (int)rayonH, null);
 
-			//Tracage des lignes de 7 pavés
-			for(int i=0;i<7;i++){
-				for(int j=0;j<4;j++){
-					if(c[2*j][i] != null){               
-						if(c[2*j][i].getEtat() == Etat.DEUX_POISSONS){
-							drawable.drawImage(deux_poissons,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
-						}
-						else if(c[2*j][i].getEtat() == Etat.UN_POISSON)
+
+
+		//Tracage des lignes de 7 pavés
+		for(int i=0;i<7;i++){
+			for(int j=0;j<4;j++){
+				if(c[2*j][i] != null){               
+					if(c[2*j][i].getEtat() == Etat.DEUX_POISSONS){
+						drawable.drawImage(deux_poissons,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
+					}
+					else if(c[2*j][i].getEtat() == Etat.UN_POISSON)
+					{
+				   		if (arbitre.getMode() == ModeDeJeu.POSE_PINGOUIN)
+				   		{
+				   			drawable.drawImage(poissonRouge,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);			   		
+				   		}
+				   		else
+				   		{
+				   			drawable.drawImage(un_poisson,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
+				   		} 
+		
+				   	}
+					else if(c[2*j][i].getEtat() == Etat.TROIS_POISSONS){
+						drawable.drawImage(trois_poissons,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
+					}
+					
+					joueur = c[2*j][i].getJoueurSurCase();
+					
+					if(joueur != null){
+						if(coupPrec.x == 2*j && coupPrec.y == i)
 						{
-					   		if (arbitre.getMode() == ModeDeJeu.POSE_PINGOUIN)
-					   		{
-					   			drawable.drawImage(poissonRouge,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);			   		
-					   		}
-					   		else
-					   		{
-					   			drawable.drawImage(un_poisson,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
-					   		} 
-			
-					   	}
-						else if(c[2*j][i].getEtat() == Etat.TROIS_POISSONS){
-							drawable.drawImage(trois_poissons,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
+							drawable.drawImage(entoure,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
 						}
-						
-						joueur = c[2*j][i].getJoueurSurCase();
-						
-						if(joueur != null){
-							if(coupPrec.x == 2*j && coupPrec.y == i)
-							{
-								drawable.drawImage(entoure,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
-							}
-							BufferedImage imageJoueur = null;
-							if(joueur == inter.joueurs[0])
-							{
-								drawable.drawImage(imageJoueur1,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
-							}
-							else if(joueur == inter.joueurs[1]){
-								drawable.drawImage(imageJoueur2,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
-							}
+						BufferedImage imageJoueur = null;
+						if(joueur == inter.joueurs[0])
+						{
+							drawable.drawImage(imageJoueur1,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
+						}
+						else if(joueur == inter.joueurs[1]){
+							drawable.drawImage(imageJoueur2,tabCase.sommetG_x(i,2*j),tabCase.sommetG_y(i,2*j),tabCase.largeur(),tabCase.hauteur(),null);
 						}
 					}
 				}
 			}
-			// case de suggestions
-			
-	
-			//tracage des lignes de 8
-			for(int i=0;i<8;i++){
-				for(int j=0;j<4;j++){
-					if(c[2*j+1][i] != null){                  
-						if(c[2*j+1][i].getEtat() == Etat.DEUX_POISSONS){
-							drawable.drawImage(deux_poissons,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
-						}
-						else if(c[2*j+1][i].getEtat() == Etat.UN_POISSON)
+		}
+		// case de suggestions
+		
+
+		//tracage des lignes de 8
+		for(int i=0;i<8;i++){
+			for(int j=0;j<4;j++){
+				if(c[2*j+1][i] != null){                  
+					if(c[2*j+1][i].getEtat() == Etat.DEUX_POISSONS){
+						drawable.drawImage(deux_poissons,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
+					}
+					else if(c[2*j+1][i].getEtat() == Etat.UN_POISSON)
+					{
+						if (arbitre.getMode() == ModeDeJeu.POSE_PINGOUIN)
 						{
-							if (arbitre.getMode() == ModeDeJeu.POSE_PINGOUIN)
-							{
-								drawable.drawImage(poissonRouge,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
-							}
-							else
-							{
-								drawable.drawImage(un_poisson,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
-							}
+							drawable.drawImage(poissonRouge,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
 						}
-						else if(c[2*j+1][i].getEtat() == Etat.TROIS_POISSONS){
-							drawable.drawImage(trois_poissons,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
+						else
+						{
+							drawable.drawImage(un_poisson,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
 						}
-					
-						joueur = c[2*j+1][i].getJoueurSurCase();
-					
-						if(joueur != null){
-							if(coupPrec.x == 2*j+1 && coupPrec.y == i)
-							{
-								drawable.drawImage(entoure,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
-							}
-							BufferedImage imageJoueur = null;
-							if(joueur == inter.joueurs[0])
-							{
-								drawable.drawImage(imageJoueur1,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
-							}
-							else if(joueur == inter.joueurs[1]){
-								drawable.drawImage(imageJoueur2,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
-							}
+					}
+					else if(c[2*j+1][i].getEtat() == Etat.TROIS_POISSONS){
+						drawable.drawImage(trois_poissons,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
+					}
+				
+					joueur = c[2*j+1][i].getJoueurSurCase();
+				
+					if(joueur != null){
+						if(coupPrec.x == 2*j+1 && coupPrec.y == i)
+						{
+							drawable.drawImage(entoure,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
+						}
+						BufferedImage imageJoueur = null;
+						if(joueur == inter.joueurs[0])
+						{
+							drawable.drawImage(imageJoueur1,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
+						}
+						else if(joueur == inter.joueurs[1]){
+							drawable.drawImage(imageJoueur2,tabCase.sommetG_x(i,2*j+1),tabCase.sommetG_y(i,2*j+1),tabCase.largeur(),tabCase.hauteur(),null);
 						}
 					}
 				}
-						
-			
+			}
     	} //fin affichage partie en cours
 
     }//fin methode paint
 
+
+
+
+	//
+	//Gestion du clic dans la fenetre
+	//
     public void click(int x, int y)
     { 
     	Point p = tabCase.estDansHexagone(x,y);
     	
-    	if(p.x != -1 && p.y != -1)
-    	{
+		if(p.x != -1 && p.y != -1)
+		{ //On est sur une case du jeu
+			
 			if(ArbitreManager.instance.getMode() == ModeDeJeu.POSE_PINGOUIN)
-			{	
-				if(ArbitreManager.instance.getConfiguration().estCoupPossible(new Coup(p.y, p.x, -1, -1))){
+			{ //mode pose pingouin 
+				if(ArbitreManager.instance.getConfiguration().estCoupPossible(new Coup(p.y, p.x, -1, -1)))
+				{
 					ArbitreManager.instance.getJoueurCourant().getSignalCoup().envoyerSignal(new Coup(p.y, p.x, -1, -1));
-            }
-				else
-					System.out.println("Coup illegal, pas de positionnement ici !");
-			}
-	    	else{ //mode jeu
-				if(coupPrec.x == -1 || coupPrec.y == -1){
-		    		Case [][] t = ArbitreManager.instance.getConfiguration().getTerrain();
-		    		if(t[p.x][p.y] != null){
+	        	}
+				//else
+					//System.out.println("Coup illegal, pas de positionnement ici !");
+			}//fin mode pose pingouin
+			
+			else
+			{ //mode jeu
+				if(coupPrec.x == -1 || coupPrec.y == -1)
+				{ //le coup precedent est nul
+					Case [][] t = ArbitreManager.instance.getConfiguration().getTerrain();
+					if(t[p.x][p.y] != null){
 						if(t[p.x][p.y].getJoueurSurCase() == ArbitreManager.instance.getJoueurCourant()) {
 							coupPrec.x = p.x;
 							coupPrec.y = p.y;
 						}
-		    		}
-				}
-				else { //le coup precedent est sur un pingouin du joueur
+					}
+				} //fin coup precedent nul
+				else 
+				{ //le coup precedent est sur un pingouin du joueur
 					Coup c = new Coup(coupPrec.y, coupPrec.x, p.y, p.x);
 					if(ArbitreManager.instance.getConfiguration().estCoupPossible(c))
-					{
+					{ //le coup demandé est possible
 						ArbitreManager.instance.getJoueurCourant().getSignalCoup().envoyerSignal(c);
 						coupPrec.x = p.x;
 						coupPrec.y = p.y;
 					}
-		    		else{
+					else
+					{//coup non dispo
 						System.out.println("Le coup " + c + " n'est pas pas autorisé"); 
 						Case [][] t = ArbitreManager.instance.getConfiguration().getTerrain();
-						if(t[p.x][p.y].getJoueurSurCase() == ArbitreManager.instance.getJoueurCourant()) {
-			    			coupPrec.x = p.x;
-			    			coupPrec.y = p.y;
-						}
-						else{
-			    			coupPrec = new Point(-1, -1);
-						}
-		    		}
-
-				}
-	    	}
-    	}
+						if(t[p.x][p.y].getJoueurSurCase() == ArbitreManager.instance.getJoueurCourant()) 
+						{//clic sur un pingouin du joueur
+							coupPrec.x = p.x;
+							coupPrec.y = p.y;
+						}//fin si
+						else
+						{//on annule la selecion du pingouin
+							coupPrec = new Point(-1, -1);
+						}//fin else
+					}//fin coup non dispo
+				}//fin else
+			} //fin mode jeu
+		}//fin cadre de jeu
     	else{ //on regarde si on a cliquer sur un bouton	
 			if(x > (largeur/2 - 35) && x < (largeur/2 + 35) && y < 50)
-			{
-				//System.out.println("Clic sur le bouton menu");
+			{//clic sur menu
+				//Clic sur le bouton menu;
 				if(!menuOuvert)
 				{
 					menuOuvert = true;
@@ -585,19 +576,31 @@ public class AireDeJeu extends JComponent
 						popup.setSelected(true);
 					} catch (java.beans.PropertyVetoException e) {}
 				}
-			}
-			else if(y > hauteur - (int)rayonH){
+			}//fin clic sur menu
+			else if(y > hauteur - (int)rayonH)
+			{//clic dans la bande en bas
 				if(x > largeur/4 && x < (largeur/4 + (int)rayonL*2))
 				{
-					System.out.println("Retour");
+					//Clic sur Retour
 					ArbitreManager.instance.reculerHistorique();
 				}
 				else if(x > (3*largeur/4) && x < (3*largeur/4 + (int)rayonL*2))
-				{
+				{   //clic sur refaire
 					System.out.println("Refaire");
 					ArbitreManager.instance.avancerHistorique();
 				}
-			}	
+			}//fin clic sur la bande du bas
+			else if(x > margeGauche )
+			{//clic sur le bouton aide
+				//if distance(y, centreAide) < hauteur du bouton aide
+					//afficher aide
+
+			}
+			else if(x > largeur - margeGauche)
+			{//clic sur le bouton suggestion
+				//if distance(y, centreSuggestion) < hauteur du bouton suggestion
+					//jouer coup
+			}
     	}
     	this.repaint();
     }
