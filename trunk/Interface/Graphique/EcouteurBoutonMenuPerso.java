eebd788dbc88b3c92917facc7d18265253c8b496
gx.hugo <gx.hugo@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-06-04 11:20:45 +0000

    Fix les caca en println (super devant huard...) + Fin de partie sur arbitre

--- trunk/Interface/Graphique/EcouteurBoutonMenuPerso.java
+++ trunk/Interface/Graphique/EcouteurBoutonMenuPerso.java
@@ -10,42 +10,40 @@ import Joueurs.*;
 
 
 public class EcouteurBoutonMenuPerso implements ActionListener{
-SelectionJoueurs J;
-int but;
+    SelectionJoueurs J;
+    int but;
 
-public EcouteurBoutonMenuPerso(SelectionJoueurs J ,int but)
-{
-this.J = J;
-this.but = but;
-}
+    public EcouteurBoutonMenuPerso(SelectionJoueurs J ,int but)
+    {
+        this.J = J;
+        this.but = but;
+    }
 
-public void actionPerformed(ActionEvent e)
-{
-	if(but == 1)
-	{
-	J.Ordinateur = false;
-	J.Humain = true;
-	J.Aucun = false;
-	J.selectionDifficulte();	
-	System.out.println("caca1");
-	}
-	if(but == 2)
-	{
-	J.Ordinateur = true;
-	J.Humain = false;
-	J.Aucun = false;
-	J.selectionDifficulte();
-	System.out.println("caca2");
-	}
-	if(but == 3)
-	{
-	J.Ordinateur = false;
-	J.Humain = false;
-	J.Aucun = true;
-	J.selectionDifficulte();
-	}
+    public void actionPerformed(ActionEvent e)
+    {
+        if(but == 1)
+        {
+            J.Ordinateur = false;
+            J.Humain = true;
+            J.Aucun = false;
+            J.selectionDifficulte();	
+        }
+        if(but == 2)
+        {
+            J.Ordinateur = true;
+            J.Humain = false;
+            J.Aucun = false;
+            J.selectionDifficulte();
+        }
+        if(but == 3)
+        {
+            J.Ordinateur = false;
+            J.Humain = false;
+            J.Aucun = true;
+            J.selectionDifficulte();
+        }
 	
-}
+    }
 
 
 
