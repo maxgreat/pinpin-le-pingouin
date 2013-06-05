df27a2786de7bfe40e0fbd53d2bac75185e24a02
maxime.portaz <maxime.portaz@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-06-05 11:35:03 +0000

    Ajout choix IA en cours

---
+++ Interface/Graphique/EcouteurDeBoutonMenuIA.java
@@ -0,0 +1,20 @@
+package Interface.Graphique;
+import Interface.*;
+import java.awt.event.*;
+import Arbitre.*;
+import Joueurs.*;
+
+public class EcouteurDeBoutonMenuIA implements ActionListener{
+	String message;
+	MenuSelectionIA menu;
+
+	public EcouteurDeBoutonMenuIA(String m, MenuSelectionIA menu){
+		message = m;
+		this.menu = menu;
+	}
+
+	public void actionPerformed(ActionEvent e)
+    {
+    	menu.close();	
+	}
+}
