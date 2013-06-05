d505924908dec2ae7b109def369a634ad1c372ef
gx.hugo <gx.hugo@e1f3eaec-9666-91f0-b654-9e7ceb0cee30>
2013-06-05 11:04:37 +0000

    Corrige le chargement à trois joueur

--- trunk/Interface/Graphique/SelectionJoueurs.java
+++ trunk/Interface/Graphique/SelectionJoueurs.java
@@ -13,33 +13,33 @@ import Joueurs.*;
 
 public class SelectionJoueurs{
 
-//le panel dans lequel on mettra les deux selecteurs
-JPanel joueur = new JPanel();
+    //le panel dans lequel on mettra les deux selecteurs
+    JPanel joueur = new JPanel();
 
-// les listes de difficultés des deux 
-LinkedList<String> joueurs;
-LinkedList<String> joueursPris;
-LinkedList<String> difficultees;
-//les informations sur le joueur
-boolean Ordinateur;
-boolean Humain;
-boolean Aucun;
-String nomJoueur = new String();
-String difficultésJoueur;
-//il nous faut l'ancien joueur pour pouvoir l'enlever de joueursPris
-String nomJoueurOld = new String();
-//on a besoin de menu perso pour
-MenuPerso mp;
-//les combobox
-JComboBox names;
-JComboBox diff;
-int menu;
-JPanel vide;
-JPanel buttons;
-InterfaceGraphique inter;
-Joueur	Sjoueurs []= new Joueur[4];
+    // les listes de difficultés des deux 
+    LinkedList<String> joueurs;
+    LinkedList<String> joueursPris;
+    LinkedList<String> difficultees;
+    //les informations sur le joueur
+    boolean Ordinateur;
+    boolean Humain;
+    boolean Aucun;
+    String nomJoueur = new String();
+    String difficultesJoueur;
+    //il nous faut l'ancien joueur pour pouvoir l'enlever de joueursPris
+    String nomJoueurOld = new String();
+    //on a besoin de menu perso pour
+    MenuPerso mp;
+    //les combobox
+    JComboBox names;
+    JComboBox diff;
+    int menu;
+    JPanel vide;
+    JPanel buttons;
+    InterfaceGraphique inter;
+    Joueur	Sjoueurs []= new Joueur[4];
 
-public SelectionJoueurs(JPanel joueur,  LinkedList<String> joueurs , LinkedList<String> joueursPris, LinkedList<String> difficultees,MenuPerso mp,InterfaceGraphique inter ,int menu)
+    public SelectionJoueurs(JPanel joueur,  LinkedList<String> joueurs , LinkedList<String> joueursPris, LinkedList<String> difficultees,MenuPerso mp,InterfaceGraphique inter ,int menu)
 	{
 		this.inter = inter;
 		this.menu = menu;
@@ -48,63 +48,78 @@ public SelectionJoueurs(JPanel joueur,  LinkedList<String> joueurs , LinkedList<
 		this.joueursPris = joueursPris;
 		this.difficultees=difficultees;
 		nomJoueur = joueurs.getFirst();
-		difficultésJoueur = difficultees.getFirst();
+		difficultesJoueur = difficultees.getFirst();
 		this.mp = mp;
+
+        Sjoueurs[0] = new JoueurHumain();
+        Sjoueurs[0].setNom("Atos");
+
+        Sjoueurs[1] = new JoueurCPUUniversel(3);
+        Sjoueurs[1].setNom("Ordinateur (facile)");
+
+        Sjoueurs[2] = null;
+        Sjoueurs[3] = null;
+
+        inter.setTab(Sjoueurs);
+
 	}
 
-public void selectionJoueur()
+    public void selectionJoueur()
 	{
 
-	Object [] test = joueurs.toArray();
-	buttons = new JPanel();
-	diff = new JComboBox(difficultees.toArray());
-	names = new JComboBox(test);
-	diff.setVisible(false);
-	names.setVisible(false);
-	buttons.setLayout(new GridLayout(0,3));
-	JButton b1 = new JButton("HUMAIN");
-	JButton b2 = new JButton("CPU");
-	JButton b3 = new JButton("AUCUN");
+        Object [] test = joueurs.toArray();
+        buttons = new JPanel();
+        diff = new JComboBox(difficultees.toArray());
+        names = new JComboBox(test);
+        diff.setVisible(false);
+        names.setVisible(false);
+        buttons.setLayout(new GridLayout(0,3));
+        JButton b1 = new JButton("HUMAIN");
+        JButton b2 = new JButton("CPU");
+        JButton b3 = new JButton("AUCUN");
 
-	b1.addActionListener(new EcouteurBoutonMenuPerso(this,1));
-	b2.addActionListener(new EcouteurBoutonMenuPerso(this,2));
-	b3.addActionListener(new EcouteurBoutonMenuPerso(this,3));
+        SelectionJoueur partage = new SelectionJoueur(menu);
+
+        b1.addActionListener(new EcouteurBoutonMenuPerso(this, partage, 1));
+        b2.addActionListener(new EcouteurBoutonMenuPerso(this, partage, 2));
+        b3.addActionListener(new EcouteurBoutonMenuPerso(this, partage, 3));
 	
-	buttons.add(b1);
-	buttons.add(b2);
-	buttons.add(b3);
-	names.addActionListener(new EcouteurDeBox(this,true,mp.inter,menu));
-	diff.addActionListener(new EcouteurDeBox(this,false,mp.inter,menu));
-	vide = new JPanel();
-	vide.setOpaque(false);
-	joueur.setLayout(new BoxLayout(joueur, BoxLayout.PAGE_AXIS));
-	joueur.add(buttons);
-	joueur.add(names);
-	joueur.add(diff);
-	joueur.add(vide);
+        buttons.add(b1);
+        buttons.add(b2);
+        buttons.add(b3);
+        names.addActionListener(new EcouteurDeBox(this, partage, mp.inter, menu));
+        diff.addActionListener(new EcouteurDeBox(this, partage, mp.inter, menu));
+        vide = new JPanel();
+        vide.setOpaque(false);
+        joueur.setLayout(new BoxLayout(joueur, BoxLayout.PAGE_AXIS));
+        joueur.add(buttons);
+        joueur.add(names);
+        joueur.add(diff);
+        joueur.add(vide);
 
-	joueur.setOpaque(false);
+        joueur.setOpaque(false);
 
-	Aucun = true;
-	Ordinateur=false;
-	Humain=false;
+        Aucun = true;
+        Ordinateur=false;
+        Humain=false;
 
-	selectionDifficulte();
+        selectionDifficulte(partage);
 	
 	}
 
-public void selectionDifficulte()
+    public void selectionDifficulte(SelectionJoueur type)
 	{
-	names.setVisible(Humain);
-	diff.setVisible(Ordinateur);
-	vide.setVisible(Aucun);	
+        names.setVisible(type.estJoueur());
+        diff.setVisible(type.estOrdinateur());
+        vide.setVisible(type.estAucun());	
+        
 	}
-public JPanel getPan()
+    public JPanel getPan()
 	{
-	return joueur;
+        return joueur;
 	}
 
-private URL getImage(String nom) {
+    private URL getImage(String nom) {
         ClassLoader cl = getClass().getClassLoader();
         return cl.getResource("Interface/Graphique/Img/" + nom);
     }
